465bf8c6d4de93cd54f0fbd4ad57eb9581b76212
Muhammed Swalih M <zzray485@gmail.com>
2025-03-01 18:45:50 +0530

    added basic edit and delete

--- basic_crud.py
+++ basic_crud.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import MyModel
 
 def create_item(request):
@@ -13,3 +13,19 @@ def create_item(request):
 def list_items(request):
     items = MyModel.objects.all()
     return render(request, 'appname/item_list.html', {'items':items})
+
+def edit_item(request, item_id):
+    item = get_object_or_404(MyModel, id=item_id)
+    if request.method == 'POST':
+        item.name = request.POST.get('name')
+        item.description = request.POST.get('description')
+        item.save()
+        return redirect('item_list')
+    return render(request, 'appname/item_form.html', {'item' : item})
+
+def delete_item(request, item_id):
+    item = get_object_or_404(MyModel, id=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'appname/item_confirm_delete.html', {'item' : item})
